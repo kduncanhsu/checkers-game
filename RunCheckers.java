741be6f3222456c00c4933335eea19a63cd4fbc5
Duncan Hsu <102879458+kduncanhsu@users.noreply.github.com>
2023-06-01 14:23:53 +0800

    v2

--- RunCheckers.java
+++ RunCheckers.java
@@ -7,16 +7,13 @@ import java.awt.*;
  * This class sets up the top-level frame and widgets for the GUI.
  * 
  * This game adheres to a Model-View-Controller design framework. This
- * framework is very effective for turn-based games. We STRONGLY
- * recommend you review these lecture slides, starting at slide 8,
- * for more details on Model-View-Controller:
- * https://www.seas.upenn.edu/~cis120/current/files/slides/lec37.pdf
- * 
+ * framework is very effective for turn-based games. 
+ 
  * In a Model-View-Controller framework, Game initializes the view,
  * implements a bit of controller functionality through the reset
  * button, and then instantiates a GameBoard. The GameBoard will
  * handle the rest of the game's view and controller functionality, and
- * it will instantiate a TicTacToe object to serve as the game's model.
+ * it will instantiate a Checkers object to serve as the game's model.
  */
 public class RunCheckers implements Runnable {
     public void run() {
